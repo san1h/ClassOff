d4735838c3f620a87da119089bb7ee1868418852
VisualCodeScriptDLL <dragoecking@gmail.com>
2025-03-17 09:16:26 -0500

    arreglando codigos de los archivos

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+
+// Esperamos a que el documento esté completamente cargado
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('changeColorBtn'); // Obtenemos el botón
+
+    // Función para cambiar el color de fondo
+    button.addEventListener('click', function() {
+        document.body.style.backgroundColor = document.body.style.backgroundColor === 'lightblue' ? '#f4f4f4' : 'lightblue';
+    });
+});
